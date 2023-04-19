5aa8937b76b251c2178d9a87ecfd91203fd8222d
Ivlev Nikita <evilevnik@gmail.com>
2023-04-19 18:30:09 +0600

    Initial Commit

---
+++ TFP userPage/TFP_userPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  TFP_userPageApp.swift
+//  TFP userPage
+//
+//  Created by Nikita Ivlev on 19/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TFP_userPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
